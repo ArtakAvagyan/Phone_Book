35fbfb885fc73c9ae8a20b2096f0afe3e8e4ce00
Artak Avagyan <92084060+ArtakAvagyan@users.noreply.github.com>
2022-01-24 02:06:16 +0400

    Update source.cpp

--- Source/source.cpp
+++ Source/source.cpp
@@ -3,7 +3,8 @@
 Number::Number (const std::string name, const std::string lastName = "" , const std::string address = "", const std::string number = "")
 : firstName_{name} , lastName_{lastName} , address_{address} , number_{number}{}
 
-Number::Number (const Number& rhs):firstName_{rhs.firstName_} , lastName_{rhs.lastName_} , address_{rhs.address_} , number_{rhs.number_}{}
+Number::Number (const Number& rhs):firstName_{rhs.firstName_} , lastName_{rhs.lastName_} ,
+					address_{rhs.address_} , number_{rhs.number_}{}
 
 
 Number::Number (Number&& rhs):firstName_{std::move(rhs.firstName_)} , lastName_{std::move(rhs.lastName_)} ,
@@ -30,6 +31,7 @@ Number& Number::operator=(Number&& rhs)
 	number_ = std::move(rhs.number_);
 	return *this;
 }
+
 bool Number::operator< (const Number& rhs)const
 {
 	return this->firstName_<rhs.firstName_;
@@ -44,10 +46,12 @@ bool Number::operator== (const Number& rhs)const
 {
 	return this->firstName_==rhs.firstName_;
 }
+
 bool Number::operator>= (const Number& rhs)const
 {
 	return !(this->firstName_<rhs.firstName_);
 }
+
 bool Number::operator<= (const Number& rhs)const
 {
 	return !(this->firstName_>rhs.firstName_);
@@ -64,7 +68,6 @@ std::istream& operator>> (std::istream& is , Number& rhs)
 	is >> rhs.firstName_ >> rhs.lastName_ >> rhs.address_ >> rhs.number_ ;
 	return is;
 }
-
 
 bool Number::findNameSurname(const std::string& name,const std::string& surname)const
 {
